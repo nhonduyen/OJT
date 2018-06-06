6ec7254068f9ab4ff289b4150f8fcf748594a4ec
nhonduyen <nhonduyen@gmail.com>
2018-06-06 10:48:27 +0700

    + Export to excel using Epplus

--- OJT/Controllers/HomeController.cs
+++ OJT/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -21,7 +23,7 @@ namespace OJT.Controllers
             HISTORY his = new HISTORY();
             COURSE course = new COURSE();
             EMPLOYEE em = new EMPLOYEE();
-     
+
             List<COURSE> courses = course.Select();
             if (COURSE_ID == -1)
             {
@@ -201,6 +203,127 @@ namespace OJT.Controllers
         {
             Session["Culture"] = new CultureInfo(lang);
             return Redirect(returnUrl);
-        }  
+        }
+
+        public ActionResult Export(int COURSE_ID = -1, string MENTOR = "", string EMP_ID = "")
+        {
+            if (Session["Username"] == null)
+                return RedirectToAction("Login", "Home");
+            var fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx";
+            var template = Server.MapPath("~/Upload/Template/Export/OJT_EXP1.xlsx");
+            var dept = "";
+            HISTORY his = new HISTORY();
+            COURSE course = new COURSE();
+            EMPLOYEE em = new EMPLOYEE();
+
+            List<COURSE> courses = course.Select();
+            if (COURSE_ID == -1)
+            {
+                if (course != null && courses.Count > 0)
+                {
+                    COURSE_ID = courses[0].ID;
+                }
+                else
+                {
+                    COURSE_ID = 0;
+                }
+            }
+            if (Convert.ToInt32(Session["Role"].ToString()) == 0)
+            {
+                var isMentee = em.IsMentee(COURSE_ID, Session["Username"].ToString());
+                var isMentor = em.IsMentor(COURSE_ID, Session["Username"].ToString());
+                if (isMentee == 1 && string.IsNullOrWhiteSpace(EMP_ID))
+                {
+                    EMP_ID = Session["Username"].ToString();
+                    MENTOR = em.GetListMentor(COURSE_ID).FirstOrDefault().ID;
+                }
+                else if (isMentor == 1 && string.IsNullOrWhiteSpace(MENTOR))
+                {
+                    MENTOR = Session["Username"].ToString();
+                }
+                else
+                {
+                    EMP_ID = "0";
+                    MENTOR = "0";
+                }
+            }
+            if (Convert.ToInt32(Session["Role"].ToString()) == 2)
+            {
+                dept = Session["Dept"].ToString();
+            }
+
+            var lstHis = his.GetHistory(MENTOR, EMP_ID, COURSE_ID, dept);
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(template)))
+            {
+                ExcelWorksheet ws = package.Workbook.Worksheets.FirstOrDefault();
+                for (int i = 0; i < lstHis.Count; i++)
+                {
+                    var step = i;
+                    int count = lstHis[i].CNT_EMP;
+                    var cntCourse = lstHis[i].CNT_COURSE;
+                    var history = lstHis[i];
+                    var startIndex = i + 4;
+                    ws.Cells["B" + startIndex].Value = history.EMP_ID;
+                    ws.Cells["C" + startIndex].Value = history.EMP_NAME;
+                    ws.Cells["D" + startIndex].Value = history.DEPARTMENT;
+                    ws.Cells["E" + startIndex].Value = history.PERIOD;
+                    ws.Cells["F" + startIndex].Value = history.STATUS;
+                    ws.Cells["G" + startIndex].Value = history.SUBJECT;
+                    ws.Cells["H" + startIndex].Value = history.SUB_LEVEL;
+                    ws.Cells["I" + startIndex].Value = history.START_DT == null ? "" : history.START_DT.ToString("yyyy-MM-dd");
+                    ws.Cells["J" + startIndex].Value = history.END_DT == null ? "" : history.END_DT.ToString("yyyy-MM-dd");
+                    ws.Cells["K" + startIndex].Value = history.APPROVE;
+                    ws.Cells["L" + startIndex].Value = history.REC_START_DT == null ? "" : history.REC_START_DT.ToString("yyyy-MM-dd");
+                    ws.Cells["M" + startIndex].Value = history.REC_END_DT == null ? "" : history.REC_END_DT.ToString("yyyy-MM-dd");
+                    ws.Cells["N" + startIndex].Value = history.MANAGER_CMT;
+                    ws.Cells["O" + startIndex].Value = history.HR_CMT;
+                    ws.Cells["P" + startIndex].Value = history.TEST_TIME == null ? "" : history.TEST_TIME.ToString("yyyy-MM-dd");
+                    ws.Cells["Q" + startIndex].Value = history.SCORE;
+                    ws.Cells["R" + startIndex].Value = history.RESULT_LEVEL;
+
+                }
+                for (int i = 0; i < lstHis.Count; i++)
+                {
+                    var step = i;
+                    int count = lstHis[i].CNT_EMP;
+                    var cntCourse = lstHis[i].CNT_COURSE;
+                    var history = lstHis[i];
+                    var startIndex = i + 4;
+                    ws.Cells["B" + startIndex + ":B" + (startIndex + count - 1).ToString()].Merge = true;
+                    ws.Cells["B" + startIndex].Value = history.EMP_ID;
+                    ws.Cells["C" + startIndex + ":C" + (startIndex + count - 1).ToString()].Merge = true;
+                    ws.Cells["C" + startIndex].Value = history.EMP_NAME;
+                    ws.Cells["D" + startIndex + ":D" + (startIndex + count - 1).ToString()].Merge = true;
+                    ws.Cells["D" + startIndex].Value = history.DEPARTMENT;
+                    i += count - 1;
+                }
+                for (int i = 0; i < lstHis.Count; i++)
+                {
+                    var step = i;
+                    int count = lstHis[i].CNT_EMP;
+                    var cntCourse = lstHis[i].CNT_COURSE;
+                    var history = lstHis[i];
+                    var startIndex = i + 4;
+                    ws.Cells["E" + startIndex + ":E" + (startIndex + cntCourse-1).ToString()].Merge = true;
+                    ws.Cells["E" + startIndex].Value = history.PERIOD;
+                    ws.Cells["Q" + startIndex + ":Q" + (startIndex + cntCourse - 1).ToString()].Merge = true;
+                    ws.Cells["Q" + startIndex].Value = history.SCORE;
+                    ws.Cells["R" + startIndex + ":R" + (startIndex + cntCourse - 1).ToString()].Merge = true;
+                    ws.Cells["R" + startIndex].Value = history.RESULT_LEVEL;
+                    i += cntCourse - 1;
+                }
+                //buffer = package.Stream as MemoryStream;
+                Byte[] fileBytes = package.GetAsByteArray();
+                Response.ClearContent();
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=" + fileName);
+                Response.BinaryWrite(fileBytes);
+                Response.Flush();
+                Response.End();
+
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- OJT/Scripts/home.js
+++ OJT/Scripts/home.js
@@ -373,39 +373,9 @@
         });
     }
     $('#btnExport').click(function (e) {
-        fnExcelReport('tbMainDefault', e);
+        window.location = $(this).attr('data-url') + "?COURSE_ID=" + $('#selPeriod').val() + "&MENTOR=" + $('#mentor').val()
+        + "&EMP_ID=" + $('#mentee').val();
         return false;
     });
-    function fnExcelReport(ID, e) {
-        var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
-        var textRange; var j = 0;
-        tab = document.getElementById(ID); // id of table
-        var filename = moment(new Date()).format("YYYYMMDDHHmmss") + ".xls";
-
-        for (j = 0 ; j < tab.rows.length ; j++) {
-            tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
-            //tab_text=tab_text+"</tr>";
-        }
-
-        tab_text = tab_text + "</table>";
-        tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
-        tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
-        tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
-
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
-        {
-            txtArea1.document.open("txt/html", "replace");
-            txtArea1.document.write(tab_text);
-            txtArea1.document.close();
-            txtArea1.focus();
-            sa = txtArea1.document.execCommand("SaveAs", true, filename);
-        }
-        else {             //other browser not tested on IE 11
-            sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
-        }
-        return (sa);
-    }
+   
 });
